c055f0f12c12b8da48ab5f956f2d450e02b2ccb3
osw16 <oswaldoj16@gmail.com>
2021-11-26 08:13:55 -0500

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+
+      <h1>CoinMarket</h1>
+
+
+    <div>
+      <tr v-for="team in CONMEBOL" :key="team.id">
+        <td>
+          <span>{{team.name}}</span>
+        </td>
+      </tr>
+    </div>
+
+    <table class="table table-hover table-dark text-light">
+        <thead>
+          <tr>
+            <th v-for="(title, index) in titles" :key="index">
+              {{ title }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(team, index) in CONMEBOL" :key="team.id">
+            <td class="text-muted">{{ index  + 1}}</td>
+            <td>
+              <img :src="team.image" :alt="team.name" style="width: 2rem" class="me-2" />
+              <span>
+                {{ team.name }}
+              </span>
+              <span class="ms-2 text-muted text-uppercase">
+                {{ team.symbol }}
+              </span>
+            </td>
+            <td>${{ team.current_price.toLocaleString() }}</td>
+            <td
+              :class="[
+                team.price_change_percentage_24h > 0
+                  ? 'text-success'
+                  : 'text-danger',
+              ]"
+            >
+              {{ team.price_change_percentage_24h.toFixed(1)}}%
+            </td>
+            <td>${{ team.total_volume.toLocaleString() }}</td>
+          </tr>
+        </tbody>
+      </table>
+
+
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import api from './api'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+   // data method define global variables
+  data() {
+    return {
+      LALIGA:[],
+      SERIEA:[],
+      EPL:[],
+      CONMEBOL:[],
+      titles: ["#", "Coin", "Price", "Price Change", "24h Volume"],
+      textSearch: "",
+    };
+  },
+  // mounted methods is executed after the instance has been mounted
+  async mounted() {
+
+    api.getLALIGA_TEAMS()
+    .then(assets =>(this.LALIGA = assets))
+    api.getSERIEA_TEAMS()
+    .then(assets =>(this.SERIEA = assets))
+    api.getEPL_TEAMS()
+    .then(assets =>(this.EPL = assets))
+    api.getCONMEBOL_TEAMS()
+    .then(assets =>(this.CONMEBOL = assets))
+  },
+  methods: {
+    searchCoin() {
+      this.filteredCoins = this.coins.filter(
+        (coin) =>
+          coin.name.toLowerCase().includes(this.textSearch.toLowerCase()) ||
+          coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase())
+      );
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/api.js
@@ -0,0 +1,54 @@
+
+// const metaverseList = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=metaverse&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=fan-token&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+
+
+async function getLALIGA_TEAMS(){
+    const getLALIGA_TEAMS = await fetch(url)
+    const json = await getLALIGA_TEAMS.json()
+    const data = json.filter(team => team.symbol === "bar" || 
+    team.symbol ==="atm" || 
+    team.symbol ==="lev" || 
+    team.symbol ==="vcf")
+    return data
+    
+}
+async function getSERIEA_TEAMS(){
+    const getSERIEA_TEAMS = await fetch(url)
+    const json = await getSERIEA_TEAMS.json()
+    const data = json.filter(team => team.symbol === "city" || 
+    team.symbol ==="AFC" || 
+    team.symbol ==="efc")
+    return data
+    
+}
+async function getEPL_TEAMS(){
+    const getEPL_TEAMS = await fetch(url)
+    const json = await getEPL_TEAMS.json()
+    const data = json.filter(team => team.symbol === "juv" || 
+    team.symbol ==="acm" || 
+    team.symbol ==="nov" || 
+    team.symbol ==="inter" || 
+    team.symbol ==="asr")
+    return data
+    
+}
+async function getCONMEBOL_TEAMS(){
+    const getCONMEBOL_TEAMS = await fetch(url)
+    const json = await getCONMEBOL_TEAMS.json()
+    const data = json.filter(team => team.symbol === "UCH" || 
+    team.symbol ==="sccp" || 
+    team.symbol ==="cai" || 
+    team.symbol ==="spfc" || 
+    team.symbol ==="mengo" || 
+    team.symbol == 'arg')
+    return data
+    
+}
+
+export default {
+    getCONMEBOL_TEAMS,
+    getLALIGA_TEAMS,
+    getSERIEA_TEAMS,
+    getEPL_TEAMS,
+}
